ef2d2be15adf6c2bccc7536b5c335ca0676a924e
dg-rblasco <rblasco@direlecgroup.com>
2018-11-28 17:17:17 +0100

    Otro paso que no se debe mergear pero que ahí esta.
    ¡Una ranita iba caminando! ¡Una ranita iba caminando! ¡Aja,aja! ¡Bender es genial!

--- src/app/pdfcreator/pdfcreator.component.ts
+++ src/app/pdfcreator/pdfcreator.component.ts
@@ -12,7 +12,7 @@ import { LazyLoadingLibraryServiceService } from '../services/lazy-loading-libra
 })
 export class PDFcreatorComponent implements OnInit {
     //tickets dinamicos vacios
-    public numberOfTickets:number = 5;
+    public numberOfTickets:number = 4;
     public tickets_almacenados = []; //este guardará las instanciaciones (objs) de la clase ticket
     //son del ejemplo basico
     public value: string = 'Codebar 1';
@@ -37,7 +37,16 @@ export class PDFcreatorComponent implements OnInit {
         //aquí declaro los objs en tickets_almacenados
         for (let NewTicketIDLocal = 0; NewTicketIDLocal < this.numberOfTickets; NewTicketIDLocal ++){
             let idLocal = NewTicketIDLocal+1;
-            this.tickets_almacenados.push( new Ticket(3,3,'Ticket_'+idLocal));
+            let randomType = Math.floor(Math.random() * (4 - 1)) + 1;
+            this.tickets_almacenados.push(
+                new Ticket(
+                    3,
+                    3,
+                    'Ticket_'+idLocal,
+                    'tipo'+randomType,
+                    {width:90,height:40}
+                )
+            );
         }
     }
     public captureScreen() {
@@ -68,21 +77,67 @@ export class PDFcreatorComponent implements OnInit {
         });
     }
 
-  ngOnInit() {}
+    /**
+     * Se calcula la disposición de etiquetas para que no se salgan fuera de una pagina.
+     */
+    public buildArrayPages(){
+        //conseguimos las dimensiones del documento
+        let dimensions = this.getSizePage(true);
+        //paginas almacenadas en este array
+        let pagesPDF = [];
+        //conseguimos las dimensiones de cada una de las etiquetas
+        // OJO --> cualquier etiqueta se ha de calcular con un height +0.6 y un with +0.6 por los margenes
+        this.tickets_almacenados.map( (ticketNow) => {
+            // hay que ir sumando los anchos en cada linea hasta comprobar que se ha pasado
+            // en cada linea se comproborá la etiqueta más larga --> sobreescribiendo una variable que tendrá el height más larog
+            // en cada salto de linea iremos sumando los altos --> si alguno se pasa abrimos nueva pagina
+            // si un alto es más alto que la propia pagina --> se queda sola en la pagina y será cortada por donde sobresalga
+            
+        });
+    }
 
+    ngOnInit() {}
 
-  loadForm(data){
-        alert('¡Chimichangas!');
-  }
+    getSizePage(get_only_number:boolean = false){
+        let setW = 0;
+        let setH = 0;
+            //leemos su height y su width
+            this.sizes.map((sizeNow)=>{
+                if(sizeNow.size === this.sizePrint){
+                    if(this.orientation_s === 'p'){
+                        setW = sizeNow.dimensions.width;
+                        setH = sizeNow.dimensions.height;
+                    }else{
+                        setH = sizeNow.dimensions.width;
+                        setW = sizeNow.dimensions.height;
+                    }
+                }
+            });
+            //retonarmos el valor
+            if(get_only_number === true)
+                return {width:setW,height:setH};
+            else
+                return {width:setW+'mm',height:setH+'mm'};
+    }
+
+    loadForm(data){
+        alert(data);
+    }
 }
 
 class Ticket{
     public number_columns:number;
     public number_rows:number;
     public value_codebar:string;
-    constructor(number_columns:number,number_rows:number,value_codebar:string){
+    public type_ticket:string;
+    public width: number;
+    public height: number;
+    constructor(number_columns:number,number_rows:number,value_codebar:string,type_ticket:string,sizeTicket:object = {width:90,height:40}){
         this.number_columns = number_columns;
         this.number_rows = number_rows;
         this.value_codebar = value_codebar;
+        this.type_ticket = type_ticket;
+        this.width = sizeTicket['width'];
+        this.height = sizeTicket['height'];
     }
 }
--- src/app/ticket/ticket.component.ts
+++ src/app/ticket/ticket.component.ts
@@ -14,12 +14,30 @@ export class TicketComponent implements OnInit {
   @Input() objTicket: any;
   @Output() loadForm: EventEmitter<object> = new EventEmitter<object>();
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
       this.colums = Array(this.objTicket.number_columns).fill().map((x,i)=>i); // [0,1,2,3,4]
       this.rows = Array(this.objTicket.number_rows).fill().map((x,i)=>i); // [0,1,2,3,4]
+      //construye el formulario
+      this.constructorForm();
+  }
+
+  private constructorForm(){
+      switch (this.objTicket.type_ticket) {
+          case 'tipo1':
+
+              break;
+          case 'tipo2':
+
+              break;
+          case 'tipo3':
+
+              break;
+          default:
+
+              break;
+      }
   }
 
   clickTrigger() {
